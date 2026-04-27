16e3dd325c584bcb83fedddf952f0fc17fbfe9bf
IvanWu-7 <iwuzero77@gmail.com>
2026-04-27 15:54:29 +0200

    Fet
    Implement main function to demonstrate BinaryTree operations including insertion, preorder, and postorder traversal.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "BinaryTree.h"
+#include "Position.h"
+
+using namespace std;
+
+void mainExercici1() {
+    BinaryTree<int, int> tree1;
+
+    int testKeys[]   = {2, 0, 45, 76, 5, 3, 40};
+    int testValues[] = {5, 5, 1, 88, 99, 12, 9, 11};  
+
+    for (int i = 0; i < 7; i++) {        
+        cout << "Inserta a l'arbre la key " << testKeys[i] 
+             << " amb valor " << testValues[i] << endl;
+        tree1.insert(testKeys[i], testValues[i]);
+    }
+
+    cout << "Preorder = [";
+    tree1.printPreOrder();
+    cout << "]" << endl;
+
+    cout << "Postorder = [";
+    tree1.printPostOrder();
+    cout << "]" << endl;
+
+    BinaryTree<int, int> tree2(tree1);
+    cout << "identics " << tree1.identicalTree(tree2) << endl;
+
+    // Volvemos a imprimir Preorder después de la copia
+    cout << "Preorder = [";
+    tree1.printPreOrder();
+    cout << "]" << endl;
+
+    tree2.insert(4, 4);
+    cout << "identics " << tree2.identicalTree(tree1) << endl;
+
+    cout << "Preorder = [";
+    tree2.printPreOrder();
+    cout << "]" << endl;
+}
+
+int main() {
+    mainExercici1();
+    return 0;
+}
