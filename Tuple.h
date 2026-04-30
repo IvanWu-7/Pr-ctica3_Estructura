f8e5f4e6e07d059dc913976b685f89fc775d75b0
Ivan Wu <iwuzero77@gmail.com>
2026-04-30 12:37:48 +0000

    creat

--- Tuple.h
+++ Tuple.h
@@ -1,13 +1,14 @@
 #ifndef TUPLE_H
 #define TUPLE_H
 
-
+template <class Key, class Value>
 class Tuple{
 public:
-    Tuple();
-    Tuple(const Tuple& orig);
+    Tuple(const Key key, const Value value);
+    Tuple(const Tuple<Key, Value>& orig);
     virtual ~Tuple();
-    
+    getText() const;
+
 
 private:
         
